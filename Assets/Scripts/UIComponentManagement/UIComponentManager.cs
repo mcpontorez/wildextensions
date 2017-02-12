2950d6955739edeb1ea809d72e994357f0fbe623
mcpontorez <mcpontorez@hotmail.com>
2017-02-12 05:43:57 +0300

    Добавлен UIComponentsData.asset с Canvas.prefab и Button.prefab

--- Assets/Scripts/UIComponentManagement/UIComponentManager.cs
+++ Assets/Scripts/UIComponentManagement/UIComponentManager.cs
@@ -1,11 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WildUI.UIComponentsManagement.Data;
 
-namespace WildUI.UIComponentsManagement.Data
+namespace WildUI.UIComponentsManagement
 {
-    public class UIComponentManager
+    public static class UIComponentManager
     {
+        private static UIComponentsData _components;
 
+        public static UIComponentsData Components
+        {
+            get
+            {
+                if(!_components)
+                {
+                    _components = Resources.Load<UIComponentsData>(" ");
+                }
+                return _components;
+            }
+        }
     }
 }
