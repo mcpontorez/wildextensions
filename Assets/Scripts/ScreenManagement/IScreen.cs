658ce9afa227113f807fb5d1c36d3dd8c8875815
mcpontorez <mcpontorez@hotmail.com>
2017-02-15 11:11:10 +0300

    Добавлен IScreen.cs и ScreenBase

---
+++ Assets/Scripts/ScreenManagement/IScreen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WildUI.ScreenManagement
+{
+    public interface IScreen
+    {
+        void Init();
+
+        void Show();
+
+        void Hide();
+
+        void Destroy();
+    }
+}
---
+++ Assets/Scripts/ScreenManagement/ScreenBase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using WildUI.ScreenManagement.Data;
+
+namespace WildUI.ScreenManagement
+{
+    public abstract class ScreenBase : IScreen
+    {
+        protected abstract string DataPath { get; }
+
+        protected ScreenData _data;
+
+        void IScreen.Init()
+        {
+            _data = Resources.Load<ScreenData>(DataPath);
+            _data = Object.Instantiate(_data);
+
+            OnInit();
+        }
+
+        protected virtual void OnInit() { }
+
+        void IScreen.Show()
+        {
+            _data.gameObject.SetActive(true);
+            OnShow();
+        }
+
+        protected virtual void OnShow() { }
+
+        void IScreen.Hide()
+        {
+            _data.gameObject.SetActive(false);
+            OnHide();
+        }
+
+        protected virtual void OnHide() { }
+
+        void IScreen.Destroy()
+        {
+            OnDestroy();
+            Object.Destroy(_data.gameObject);
+        }
+
+        protected virtual void OnDestroy() { }
+    }
+}
