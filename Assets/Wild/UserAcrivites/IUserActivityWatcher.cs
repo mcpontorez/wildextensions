1f57263bdf9d0cbf232fdc158b2b2a17bc5d5e22
mcpontorez <mcpontorez>
2023-09-04 23:18:27 +0300

    temp realization UserActivityWatcher

---
+++ Assets/Wild/UserAcrivites/IUserActivityWatcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wild.UserAcrivites
+{
+    public interface IUserActivityWatcher
+    {
+        bool IsEnabled { get; }
+        event Action UserNotActiveDetected;
+        void Enable();
+        void Disable();
+    }
+}
---
+++ Assets/Wild/UserAcrivites/UserActivityWatcher.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Wild.Systems;
+
+namespace Wild.UserAcrivites
+{
+    public sealed class UserActivityWatcher : IDisposable, IUserActivityWatcher
+    {
+        private readonly float _userActiveWaitTime;
+        public bool IsEnabled { get; private set; } = false;
+        public void Enable()
+        {
+            IsEnabled = true;
+            _isLastAnyKey = !Input.anyKey;
+            StartWaiting();
+        }
+        public void Disable()
+        {
+            IsEnabled = false;
+            StopWaiting();
+        }
+
+        private Coroutine _waiting;
+        private readonly IGameLogicUpdateSystem _gameLogicUpdateSystem;
+
+        public event Action UserNotActiveDetected;
+        public UserActivityWatcher(IGameLogicUpdateSystem gameLogicUpdateSystem, float userActiveWaitTime)
+        {
+            _gameLogicUpdateSystem = gameLogicUpdateSystem;
+            _userActiveWaitTime = userActiveWaitTime;
+
+            _gameLogicUpdateSystem.Updated += Update;
+        }
+
+        private void StopWaiting() => _gameLogicUpdateSystem.StopNullableCoroutine(_waiting);
+        private void StartWaiting()
+        {
+            StopWaiting();
+            _waiting = _gameLogicUpdateSystem.StartCoroutine(Wait());
+        }
+
+        private bool _isLastAnyKey = true;
+
+        private void Update()
+        {
+            if (IsEnabled)
+            {
+                if (Input.anyKey != _isLastAnyKey)
+                {
+                    _isLastAnyKey = Input.anyKey;
+                    if (_isLastAnyKey)
+                    {
+                        StopWaiting();
+                    }
+                    else
+                        StartWaiting();
+                }
+            }
+        }
+
+        private IEnumerator Wait()
+        {
+            yield return new WaitForSeconds(_userActiveWaitTime);
+            UserNotActiveDetected.Invoke();
+        }
+
+        #region IDispose implementation
+        private bool _isDisposed = false;
+
+        public void Dispose()
+        {
+            if (_isDisposed)
+                return;
+
+            _gameLogicUpdateSystem.Updated -= Update;
+
+            _isDisposed = true;
+        }
+        #endregion
+    }
+}
